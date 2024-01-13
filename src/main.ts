9607abc1b109ce235802c329932f74c7799ab9a1
AlexByte <alex.g.cortes.g@gmail.com>
2024-01-13 11:44:46 -0600

    add global filter and timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
 
 
   await app.listen(3000);
