b71ced4549b79ee2658d714e299d23f1f61b2cd6
AlexByte <alex.g.cortes.g@gmail.com>
2024-01-23 23:56:22 -0600

    update UserMSG and add PassengerMSG constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,12 +1,21 @@
 export enum RabbitMQ {
     UserQueue = 'users',
+    PassengerQueue = 'passenger',
 }
 
 export enum UserMSG {
-    CREATE = 'CREATE',
-    FIND_ALL = 'FIND_ALL',
-    FIND_ONE = 'FIND_ONE',
-    UPDATE = 'UPDATE',
-    DELETE = 'DELETE',
+    CREATE = 'CREATE_USER',
+    FIND_ALL = 'FIND_USERS',
+    FIND_ONE = 'FIND_USER',
+    UPDATE = 'UPDATE_USER',
+    DELETE = 'DELETE_USER',
     VALID_USER = 'VALID_USER',
+}
+
+export enum PassengerMSG {
+    CREATE = 'CREATE_PASSENGER',
+    FIND_ALL = 'FIND_PASSENGERS',
+    FIND_ONE = 'FIND_ONE_PASSENGER',
+    UPDATE = 'UPDATE_PASSENGER',
+    DELETE = 'DELETE_PASSENGER',
 }
